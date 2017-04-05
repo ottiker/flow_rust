a200add4aa48aa270faa202b562a111b3aa37fe0
Adrian Ottiker <adrian@ottiker.com>
2017-04-05 12:15:28 +0200

    update test call

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use flow_rust::FsAdapter;
 fn main() {
 
     let error_prefix = "Flow-rust";
+	let default_role = "*";
 
     // get sequence id from cli
     let sequence_id = env::args().nth(1).expect("Flow-rust: Missing sequence id.");
@@ -18,13 +19,10 @@ fn main() {
     let base = canonicalize(&location).expect(&error_prefix);
     env::set_current_dir(&base).expect(&error_prefix);
 
-	let test_sequence_id = "testSequenceId";
-	let test_role = "testRole";
 	let lru_size:usize = 2;
 	let adapter = FsAdapter::new(&lru_size);
-	let flow = Flow(&adapter);
-	//(&test_sequence_id, &test_role);
-	//println!("flow event: {}, {}", e.sequence, e.role);
+	let e = Flow(&adapter)(&sequence_id, &default_role);
+	println!("flow event: {}, {}", e.sequence, e.role);
 
     println!(
         "Sequence ID: {}\nSequence Location: {:?}",
