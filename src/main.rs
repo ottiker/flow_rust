68f935ac1877b2e092b4b1100de16ffce5c892ae
Adrian Ottiker <adrian@ottiker.com>
2018-01-03 23:46:00 +0000

    refactoring flow.rs using futures

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 extern crate flow;
-extern crate flow_rust;
-extern crate lru;
 
 use std::env;
-use std::fs::canonicalize;
-use flow::flow;
-use flow_rust::{factory, FsAdapter};
-use std::collections::HashMap;
+
+// TODO loaders for sequence (json) and handler (Fn)
+
+/*struct Loader {
+    name: String
+}
+
+impl Loader {
+    fn sequence () {},
+    fn handler () {}
+}*/
 
 fn main() {
 
-    let error_prefix = "Flow-rust";
-	let default_role = "*";
-
     // get sequence id from cli
-    let sequence_id = env::args().nth(1).expect("Flow-rust: Missing sequence id.");
+    let sequence = env::args().nth(1).expect("Flow: Missing sequence id.");
 
-    // get and set working dir
-    let location = env::args().nth(2).unwrap_or(String::from("."));
-    let base = canonicalize(&location).expect(&error_prefix);
-    env::set_current_dir(&base).expect(&error_prefix);
-
-	// init flow with an adapter and emit first sequence
-	let adapter: Box<FsAdapter<HashMap<&str, isize>>> = factory();
-	flow(adapter)(&sequence_id, &default_role);
+    // create new flow instance and emit first sequence
+    flow::new(/* pass loader*/).emit(sequence);
 }
